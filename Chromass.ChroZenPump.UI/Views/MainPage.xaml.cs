2d57efd374863d0b39b470a876d62345713f2841
Jeong, Kyung Hoon <khjung@youngincm.com>
2022-09-30 18:02:12 +0900

    UI

--- Chromass.ChroZenPump.UI/Views/MainPage.xaml.cs
+++ Chromass.ChroZenPump.UI/Views/MainPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Chromass.ChroZenPump.UI.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Chromass.ChroZenPump.UI.Views;
@@ -15,5 +15,23 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+
+        ViewModel.APIViewModel.ConnectionViewModel.PropertyChanged += ConnectionViewModel_PropertyChanged;
+    }
+
+    private void ConnectionViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (ViewModel.APIViewModel.ConnectionViewModel.IsVisible)
+        {
+            VisualStateManager.GoToState(this, "NotConnected", false);
+
+            //ConnectionPage.Opacity = 1;
+            //MainView.Visibility = Microsoft.UI.Xaml.Visibility.Collapsed;
+        }
+        else if (ConnectionPage.Opacity == 1)
+        {
+            VisualStateManager.GoToState(this, "Connected", true);
+//            ConnectionPageAnimation.Begin();
+        }
     }
 }
